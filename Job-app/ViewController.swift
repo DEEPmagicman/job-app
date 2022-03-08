7eb3ba46567f419ff6f4271c644c5ddcc7f58fbe
Deep <deep.g@squareinfosotf.com>
2022-03-08 21:55:38 +0530

    Initial Commit

---
+++ Job-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Job-app
+//
+//  Created by Square Infosoft on 08/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
