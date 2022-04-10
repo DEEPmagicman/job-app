2838d68812a6f90f00b241b72191370bf651deac
Deep <deepgandhi999@gmail.com>
2022-04-10 11:56:10 +0530

    add: account

---
+++ Job-app/ViewControllers/AccountViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AccountViewController.swift
+//  Job-app
+//
+//  Created by Square Infosoft on 10/04/22.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    @IBOutlet weak var appliedView: UIView!
+    @IBOutlet weak var settingView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.appliedView.layer.cornerRadius = 15
+        self.settingView.layer.cornerRadius = 15
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.onClickApplied))
+        self.appliedView.isUserInteractionEnabled = true
+        self.appliedView.addGestureRecognizer(tap)
+        
+        let tap2 = UITapGestureRecognizer(target: self, action: #selector(self.onClickSetting))
+        self.settingView.isUserInteractionEnabled = true
+        self.settingView.addGestureRecognizer(tap2)
+    }
+    
+    @objc private func onClickApplied(sender:UITapGestureRecognizer) {
+        
+    }
+    
+    @objc private func onClickSetting(sender:UITapGestureRecognizer) {
+        
+    }
+
+}
