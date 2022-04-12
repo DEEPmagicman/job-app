80c09bd34dd1594e69d60297289aac9ab2953436
Deep <deepgandhi999@gmail.com>
2022-04-12 17:46:53 +0530

    change: UI

--- Job-app/ViewControllers/LoginViewController.swift
+++ Job-app/ViewControllers/LoginViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var registerHereLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.onClickRegister))
+        self.registerHereLbl.isUserInteractionEnabled = true
+        self.registerHereLbl.addGestureRecognizer(tap)
+    }
+    
+    @objc private func onClickRegister(sender:UITapGestureRecognizer) {
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: String(describing: RegistrationViewController.self)) as! RegistrationViewController
+        self.navigationController?.pushViewController(vc, animated: true)
     }
     
     @IBAction func onClickLogin(_ sender: Any) {
